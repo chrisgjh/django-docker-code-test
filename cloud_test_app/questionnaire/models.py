fcabc1ef0ddded67b526912818f781157771a8ed
Jiahao Guo <JiahaoG@datacom.co.nz>
2021-09-07 11:27:00 +1200

    Models added for the app

--- cloud_test_app/questionnaire/models.py
+++ cloud_test_app/questionnaire/models.py
@@ -1,1 +1,33 @@
 from django.db import models
+from django.db.models.fields import CharField
+
+MONTHS = (
+    ('January', 'JANUARY'),
+    ('February', 'FEBRUARY'),
+    ('March', 'MARCH'),
+    ('April', 'APRIL'),
+    ('May', 'MAY'),
+    ('June', 'JUNE'),
+    ('July', 'JULY'),
+    ('August', 'AUGUST'),
+    ('September', 'SEPTEMBER'),
+    ('October', 'OCTOBER'),
+    ('November', 'NOVEMBER'),
+    ('December', 'DECEMBER'),
+)
+
+class FavMonth(models.Model):
+    month = CharField(max_length=20, default='December', choices=MONTHS, null=False)
+
+DAYOFWEEK = (
+    ('Monday', 'MONDAY'),
+    ('Tuesday', 'TUESDAY'),
+    ('Wednesday', 'WEDNESDAY'),
+    ('Thursday', 'THURSDAY'),
+    ('Friday', 'FRIDAY'),
+    ('Saturday', 'SATURDAY'),
+    ('Sunday', 'SUNDAY'),
+)
+
+class FavDayOfWeek(models.Model):
+    dayofweek = CharField(max_length=20, default='Friday', choices=DAYOFWEEK, null=False)
--- cloud_test_app/questionnaire/views.py
+++ cloud_test_app/questionnaire/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import FormView
 
 def index(request):
     # TODO: make this a real number:
@@ -9,6 +9,7 @@ def index(request):
         "num_answers": num_answers,
     }
     return render(request, "questionnaire/index.html", context)
+
 
 def questionnaire(request):
     return render(request, "questionnaire/questionnaire.html")
